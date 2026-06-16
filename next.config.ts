a971642aac031547af8c438d235d61e731f8e390
Gabriel Claudino Pimentel <claudinopimentelgabriel@gmail.com>
2026-06-15 23:49:45 -0300

    Adiciona basePath no next.config.ts para corrigir rotas no GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  
   images: {
     unoptimized: true,
   },
+  basePath: "/exercicio-requisicao-assincrona",
 };
 
 export default nextConfig;
